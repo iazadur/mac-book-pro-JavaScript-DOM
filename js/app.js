cefa354258b338f4189e16c30433ef504e6087e7
iazadur <iamazadur@gmail.com>
2021-08-21 01:02:07 +0600

    update pomo code function

--- js/app.js
+++ js/app.js
@@ -9,46 +9,67 @@ const totalPrice = document.getElementById('total-price')
 
 // total Price Calculate using function 
 function totalCost() {
-    totalPrice.innerText = parseFloat(exMemory.innerText) + parseFloat(exStorage.innerText) + parseFloat(delivery.innerText) + parseFloat(bestPrice.innerText)
-    
-    
+    totalPrice.innerText = parseFloat(exMemory.innerText) + parseFloat(exStorage.innerText) + parseFloat(delivery.innerText) + parseFloat(bestPrice.innerText)  
 }
 
+
+// set extra Memory cost
+function setExMemory(price) {
+    exMemory.innerText = price
+}
+// set Extra SSD Cost
+function setExSsd(price) {
+    exStorage.innerText = price
+}
+// set Extra SSD Cost
+function setExDelivery(price) {
+    delivery.innerText = price
+}
 // memory cost
 
-const memory8 = document.getElementById('8-memory').addEventListener('click', function () {
-    exMemory.innerText = 0
+document.getElementById('8-memory').addEventListener('click', function () {
+    setExMemory(0)
     totalCost()
 })
-const memory16 = document.getElementById('16-memory').addEventListener('click', function () {
-    exMemory.innerText = 180
+document.getElementById('16-memory').addEventListener('click', function () {
+    setExMemory(180)
+
     totalCost()
 })
 
 // Storage Cost 
 
 document.getElementById('256-ssd').addEventListener('click', function () {
-    exStorage.innerText = 0
+    setExSsd(0)
     totalCost()
 })
 document.getElementById('512-ssd').addEventListener('click', function () {
-    exStorage.innerText = 100
+    setExSsd(100)
     totalCost()
 })
 document.getElementById('1-ssd').addEventListener('click', function () {
-    exStorage.innerText = 180
+    setExSsd(180)
     totalCost()
 })
 
 // Delivery charge 
 
 document.getElementById('free-delivery').addEventListener('click', function () {
-    delivery.innerText = 0
+    setExDelivery(0)
     totalCost()
 })
 
 document.getElementById('notfree-delivery').addEventListener('click', function () {
-    delivery.innerText = 20
+    setExDelivery(20)
     totalCost()
 })
 
+
+// pomo code 
+document.getElementById('pomo').addEventListener('click', function () {
+    const pomoCode = document.getElementById('pomo-code')
+    if (pomoCode.value == 'stevekaku') {
+        document.getElementById('total').innerText = parseFloat(totalPrice.innerText) - parseFloat(totalPrice.innerText) * 0.2
+        pomoCode.value = ''
+    }
+})
